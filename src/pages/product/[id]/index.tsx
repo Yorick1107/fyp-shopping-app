ba6dc850e91a5bdb62f35a75c72d2dd4bb97ec77
Yorick1107 <130445525+Yorick1107@users.noreply.github.com>
2023-04-11 15:39:42 +0000

    update product details page

--- src/pages/product/[id]/index.tsx
+++ src/pages/product/[id]/index.tsx
@@ -1,4 +1,5 @@
 import { type NextPage } from "next";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 import { api } from "~/utils/api";
@@ -11,7 +12,47 @@ const ProductDetails: NextPage = () => {
 
   return (
     <>
-      <code className="whitespace-pre">{JSON.stringify(product, undefined, 2)}</code>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#eed991] to-[#ccf7f4] bg-fixed">
+        {product ? (
+          <>
+            <div className="relative h-48 w-48 overflow-hidden">
+              {product.image ? (
+                <Image src={product.image} fill alt="product image" />
+              ) : (
+                <></>
+              )}
+            </div>
+
+            <h1 className="text-xl font-bold">{product.name}</h1>
+            <p className="text-lg font-bold">
+              ${product.prices[0]?.price || "-"}
+            </p>
+
+            {product.prices.map((price) => (
+              <>
+                <a
+                  key={product.id}
+                  className="flex max-w-xs flex-row gap-4 rounded-xl bg-white/10 p-4 text-slate-800 hover:bg-white/20"
+                  href={price.url || ""}
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <div className="text-lg font-bold text-slate-600">
+                    ${price.price}
+                  </div>
+                  <div className="text-base font-bold">{price.name}</div>
+                </a>
+              </>
+            ))}
+
+            <code className="whitespace-pre">
+              {JSON.stringify(product, undefined, 2)}
+            </code>
+          </>
+        ) : (
+          <>Loading</>
+        )}
+      </main>
     </>
   );
 };
